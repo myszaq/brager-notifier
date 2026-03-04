5c6a0693050de4733c1b80012fe50951f30b7424
Michał Myszkowski <myszaq85@gmail.com>
2026-03-04 12:33:41 +0100

    Added implementation of BrowserClient class - alternative to SeleniumBase

---
+++ services/browser_client.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+from utils.config_provider import ConfigProvider
+from utils.logger import logger
+
+
+class BrowserClient:
+
+    def open(self, url: str):
+        self.driver.get(url)
+
+    def refresh(self):
+        self.driver.refresh()
+
+    def __init__(self, browser: str = None, driver_path: str = None, headless: bool = True):
+        self._browser_name = browser or ConfigProvider.get_browser_config_option("browser_name")
+        self._driver_path = driver_path or ConfigProvider.get_browser_config_option("driver_path")
+        self._headless = headless
+        self.driver = None
+
+    def __enter__(self):
+        if self._browser_name == "chrome":
+            options = ChromeOptions()
+            if self._headless:
+                options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
+            # disable automation bar (what you already use)
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option("useAutomationExtension", False)
+            options.add_argument("--disable-blink-features=AutomationControlled")
+
+            # disable password manager & save password popup
+            prefs = {
+                "credentials_enable_service": False,
+                "profile.password_manager_enabled": False
+            }
+            options.add_experimental_option("prefs", prefs)
+
+            service = ChromeService(self._driver_path) if self._driver_path else ChromeService()
+            try:
+                self.driver = webdriver.Chrome(service=service, options=options)
+            except WebDriverException as e:
+                logger.error("Chrome driver could not start: %s", e)
+                raise
+        elif self._browser_name == "firefox":
+            options = FirefoxOptions()
+            if self._headless:
+                options.add_argument("--headless")
+            service = FirefoxService(self._driver_path) if self._driver_path else FirefoxService()
+            try:
+                self.driver = webdriver.Firefox(service=service, options=options)
+            except WebDriverException as e:
+                logger.error("Firefox driver could not start: %s", e)
+                raise
+        else:
+            raise ValueError(f"Unsupported browser: {self._browser_name}")
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.driver:
+            self.driver.quit()
+            self.driver = None
