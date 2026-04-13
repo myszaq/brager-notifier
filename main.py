230627fc6906be20847c883f35c45714ff1b44ea
Michał Myszkowski <myszaq85@gmail.com>
2026-04-13 19:16:52 +0200

    Added app version

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from services.controller import Controller
 from utils.logger import logger
 
 if __name__ == "__main__":
-    logger.info("BragerReportingTool app started.")
+    logger.info("BragerReportingTool v.1.6 app started.")
     Controller().execute()
     logger.info("BragerReportingTool app finished.")
