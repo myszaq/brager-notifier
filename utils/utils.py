d1afd1642b379e6b11f26e8e412b1e51d96ea977
Michał Myszkowski <myszaq85@gmail.com>
2026-04-13 19:11:19 +0200

    Added new utils method - run_method_with_retry

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,33 @@
+import time
+from typing import Callable
+from selenium.common import ElementClickInterceptedException, StaleElementReferenceException, TimeoutException
+from .logger import logger
+
+RETRYABLE_EXCEPTIONS = (
+    TimeoutException,
+    StaleElementReferenceException,
+    ElementClickInterceptedException,
+)
+
+
 def get_raw_temperature(input_str: str) -> str:
     return input_str.replace("°C", "")
+
+
+def run_method_with_retry(function: Callable, *args, on_retry: Callable | None = None, attempts: int = 2):
+    last_exception = None
+
+    for i in range(attempts):
+        try:
+            return function(*args)
+        except RETRYABLE_EXCEPTIONS as e:
+            last_exception = e
+            logger.warning(f"Method {function.__qualname__} failed on attempt #{i + 1}: {e}")
+
+            if i < attempts - 1:
+                if on_retry is not None:
+                    on_retry()
+                time.sleep(0.5)
+
+    logger.error(f"Method {function.__qualname__} failed after {attempts} attempts!")
+    raise last_exception
